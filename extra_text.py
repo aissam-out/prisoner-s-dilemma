c01419e172e6efaa409fbff288dd85e2be3a05c9
aissam-out <aissam.outchakoucht@gmail.com>
2021-03-08 11:24:31 +0100

    extra text

--- extra_text.py
+++ extra_text.py
@@ -1,1 +1,6 @@
 intro = "The prisoner's dilemma is a standard example in game theory.\nIt presents a situation where two parties, separated and unable to communicate, must each choose between cooperating with the other or not. The highest reward for each party occurs when both parties choose to cooperate. More specifically:\n- If they both betray each other, they get nothing.\n- If one cooperates whereas the other defects, the former get 5 points while the latter get nothing.\n- If they both cooperate, each of them get 3 points.\n\nLet's play now!\n"
+
+c_choices = ["C", "Cooperate", "c", "cooperate"]
+d_choices = ["D", "Defect", "d", "defect"]
+yes_intents = ["yes", "yeah", "sure", "y", "Y", "YES", "Yes", "Yeah", "Sure"]
+no_intents = ["no", "nah", "n", "N", "No", "NO", "Nah", "NAH"]
